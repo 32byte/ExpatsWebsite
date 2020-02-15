8b3243b513f4265fc7a6f7a9f2395b1c54bda193
byteco <xlebedenko@gmail.com>
2020-02-15 17:22:20 +0100

    changed menu bar colors

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -5,11 +5,8 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css']
 })
+
 export class TestComponent implements OnInit {
-
   constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() { }
 }
