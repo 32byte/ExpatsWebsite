a9f7be153a0cc035fc6bafa2b88f3f138e0c7359
32byte <xlebedenko@gmail.com>
2020-02-20 19:07:34 +0100

    Change tab size on scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   constructor() {
-    /* ToDo: calculate the width w/ the anchors */
+    /* Update the progressbar width on scroll */ 
     $(window).scroll(function() {
       var scroll = $(window).scrollTop(),
           dh = $(document).height(),
           h = $(window).height(),
           scrollPercent = (scroll / (dh - h)) * 100;
       $('#progressbar').css('width', scrollPercent + '%');
-      this.console.log(dh);
-      this.console.log(scroll);
+
+      var index = 0;
+      $('section').each(function (i) {
+        if(scroll >= $('section').eq(i).position().top - 56)
+          index = i;
+      });
+
+      $('.active').removeClass();
+      $('.nav-item a').eq(index).addClass('active');
+    });
+
+    /* Update the progressbar width on resize */ 
+    $(window).resize(function () {
+      var scroll = $(window).scrollTop(),
+          dh = $(document).height(),
+          h = $(window).height(),
+          scrollPercent = (scroll / (dh - h)) * 100;
+      $('#progressbar').css('width', scrollPercent + '%');
+
+      var index = 0;
+      $('section').each(function (i) {
+        if(scroll >= $('section').eq(i).position().top - 56 * index)
+          index = i;
+      });
+
+      $('.nav-item#active').removeAttr('id');
+      $('.nav-item').eq(index).attr('id', 'active');
     });
   }
 }
